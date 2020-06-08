9190650d9cb394ab002a3bdc4e68ab6cfcb34a51
Dolgopolova Maria <dolgopolovamariia@gmail.com>
2020-06-08 08:47:22 +0300

    test commit

--- chat_grpc/src/main/java/grpcchat/Client.java
+++ chat_grpc/src/main/java/grpcchat/Client.java
@@ -5,7 +5,7 @@ import io.grpc.stub.StreamObserver;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-// Client for gRPC chat
+//Client for gRPC chat
 public class Client {
     private String address;
     private int port;
